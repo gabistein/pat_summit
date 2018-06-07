75dae02c48549fd077ae1a34facd5db8832c8afe
Gabi Stein <ghstein@live.unc.edu>
2018-06-06 20:38:45 -0700

    Add files via upload
    python files scraped for seasons and then individual players. also uploaded resulting csv pre-join.

---
+++ UNCSeason.py
@@ -0,0 +1,9 @@
+class UNCSeason:
+	def __init__(self, year, conf, winPercent, averagePts, madeNCAA, coachName):
+		self.year=""
+		self.conf=""
+		self.coachName=""
+		self.winPercent=0
+		self.averagePts=0
+		
+		
