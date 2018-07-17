f41e0e972ae02bd87e650a445071b247429f4027
Gabi Stein <ghstein@live.unc.edu>
2018-07-17 01:55:24 -0400

    Delete UNCSeason.py
    Cleaning up repo

--- UNCSeason.py
+++
@@ -1,9 +0,0 @@
-class UNCSeason:
-	def __init__(self, year, conf, winPercent, averagePts, madeNCAA, coachName):
-		self.year=""
-		self.conf=""
-		self.coachName=""
-		self.winPercent=0
-		self.averagePts=0
-		
-		
